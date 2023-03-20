b1ea73519125c63e9400e44c4b5638bf7e711067
tlecae <lecaethomas4@gmail.com>
2023-03-20 18:34:25 +0100

    harmonisation

--- code/main.py
+++ code/main.py
@@ -40,32 +40,35 @@ if not os.path.exists(output_dir):
 
 
 params_f = os.path.join(code_dir, 'param.json')
-with open(params_f, 'r') as f:
-    params = json.load(f)
 
-    if params['save_all_fields'].lower() == 'true':
-        save_all_fields = True
-    else:
-        save_all_fields = False
-    raster_name = params["raster_file_name"]
-    vector_name = params["rpg_complete_name"]
-    output_name = params["name_for_output_parcels"]
 
-# Load the raster data and vector data
-with rasterio.open(os.path.join(data_dir, raster_name)) as src:
-    #Get infos about projection (in case data don't use the same projection the code will reproject vectorial data so that it matches raster projection)
-    raster = src.read(1)
-    r_crs = src.crs
-    r_epsg = r_crs.to_epsg()
-    print('Your raster file is using crs: ', r_epsg, 'This coordinate system will be used to reproject vectorial data if not.')
-    polygons = gpd.read_file(os.path.join(data_dir, vector_name))
-    if (polygons.crs.to_epsg()!= r_epsg) :
-        print('Your polygons EPSG is : ', polygons.crs, '. It will be reprojected using raster\'s EPSG.' )
-        polygons = polygons.to_crs(r_epsg)
-        #do nothing
-    else :
-        print('Your polygons EPSG is : ', polygons.crs, '. It won\'t be reprojected.' )
+def main():
+    with open(params_f, 'r') as f:
+        params = json.load(f)
+
+        if params['save_all_fields'].lower() == 'true':
+            save_all_fields = True
+        else:
+            save_all_fields = False
+        raster_name = params["raster_file_name"]
+        vector_name = params["rpg_complete_name"]
+        output_name = params["name_for_output_parcels"]
+    # Load the raster data and vector data
+    with rasterio.open(os.path.join(data_dir, raster_name)) as src:
+        #Get infos about projection (in case data don't use the same projection the code will reproject vectorial data so that it matches raster projection)
+        raster = src.read(1)
+        r_crs = src.crs
+        r_epsg = r_crs.to_epsg()
+        print('Your raster file is using crs: ', r_epsg, 'This coordinate system will be used to reproject vectorial data if not.')
+        polygons = gpd.read_file(os.path.join(data_dir, vector_name))
+        if (polygons.crs.to_epsg()!= r_epsg) :
+            print('Your polygons EPSG is : ', polygons.crs, '. It will be reprojected using raster\'s EPSG.' )
+            polygons = polygons.to_crs(r_epsg)
+            #do nothing
+        else :
+            print('Your polygons EPSG is : ', polygons.crs, '. It won\'t be reprojected.' )
         
-    compute_indexes._compute_indexes(polygons, src, output_dir, output_name)
-    
+        compute_indexes._compute_indexes(polygons, src, output_dir, output_name)
+main()
+
 print( 'The process finished successfully.')
