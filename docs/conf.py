4e04e09be7a5fba7f1b251298e4c7ad98cd5e53a
LECAE Thomas <thomas.lecae@inrae.fr>
2023-04-12 13:24:41 +0200

    adding sphinx doc and docstring #

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,12 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../code'))
+from pathlib import Path
+
+code_dir = Path(__file__).resolve().parent.parent / 'code'
+sys.path.insert(0, str(code_dir))
+
+# sys.path.insert(0, os.path.abspath('../code'))
 
 
 # -- Project information -----------------------------------------------------
